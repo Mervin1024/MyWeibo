a8dff7258e210a34572cd68cf4cec0b91ba4e525
Mervin1024 <625046839@qq.com>
2015-07-23 18:43:26 +0800

    update

---
+++ MyWeibo/Model/PersonalModel.h
@@ -0,0 +1,13 @@
+//
+//  PersonalModel.h
+//  MyWeibo
+//
+//  Created by 马遥 on 15/7/23.
+//  Copyright (c) 2015年 NJUPT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PersonalModel : NSObject
+
+@end
---
+++ MyWeibo/Model/PersonalModel.m
@@ -0,0 +1,13 @@
+//
+//  PersonalModel.m
+//  MyWeibo
+//
+//  Created by 马遥 on 15/7/23.
+//  Copyright (c) 2015年 NJUPT. All rights reserved.
+//
+
+#import "PersonalModel.h"
+
+@implementation PersonalModel
+
+@end
